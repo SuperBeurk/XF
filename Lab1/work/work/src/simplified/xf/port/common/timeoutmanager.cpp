b906c6f529e144d3203e6ecf6fd38acbfc29556c
SuperBeurk <58221021+SuperBeurk@users.noreply.github.com>
2021-10-05 16:23:10 +0200

    aaa

--- Lab1/work/work/src/simplified/xf/port/common/timeoutmanager.cpp
+++ Lab1/work/work/src/simplified/xf/port/common/timeoutmanager.cpp
@@ -30,28 +30,44 @@ void XFTimeoutManager::start(std::function<void (uint32_t)> startTimeoutManagerT
 }
 void XFTimeoutManager::scheduleTimeout(int32_t timeoutId, int32_t interval, interface::XFBehavior *pBehavior)
 {
-     timeouts_.push_back(new XFTimeout(timeoutId,interval,pBehavior));
+    //BAsic add
+    // timeouts_.push_back(new XFTimeout(timeoutId,interval,pBehavior));
+
+     //Smart add
+     int sub=0;
+     if(timeouts_.empty())
+     {
+         timeouts_.push_front(new XFTimeout(timeoutId,interval,pBehavior));//Add first element
+     }
+     else //If we have at lest one element in the list
+     {
+         for(std::list<XFTimeout *>::iterator it=timeouts_.begin();it!=timeouts_.end();it++)//Parcour the list
+         {
+             if((*it)->getRelTicks()>interval||(*it)==timeouts_.back())
+             {
+                 it=timeouts_.insert(it,new XFTimeout(timeoutId,interval,pBehavior));//Add new Timeout
+                 for(;it!=timeouts_.end();it++)//Set all tick after this element
+                 {
+                     (*it)->setRelTicks((*it)->getRelTicks()-sub);
+                     sub+=(*it)->getRelTicks();
+                 }
+                 return;
+             }
+             else
+             {
+                 sub+=(*it)->getRelTicks();
+             }
+         }
+     }
 
 }
 void XFTimeoutManager::unscheduleTimeout(int32_t timeoutId, interface::XFBehavior *pBehavior)
 {
-    /*for(std::list<XFTimeout *>::iterator it=timeouts_.begin();it!=timeouts_.end();it++)
-    {
-        if(((*it)->getId()==timeoutId)&&(((*it)->getBehavior())==pBehavior))
-        {
-            timeouts_.erase(it);
-            break;
-        }
-    }
-    for(XFTimeout* timeout : timeouts_)
-    {
 
-    }*/
 }
 void XFTimeoutManager::tick()
 {
-
-    for(std::list<XFTimeout *>::iterator it=timeouts_.begin();it!=timeouts_.end();it++)
+    /*for(std::list<XFTimeout *>::iterator it=timeouts_.begin();it!=timeouts_.end();it++)
     {
         if((*it)->getRelTicks()==0)
         {
@@ -65,7 +81,20 @@ void XFTimeoutManager::tick()
             (*it)->substractFromRelTicks(tickInterval_);
         }
 
+    }*/
+    if(timeouts_.empty()==0)
+    {
+        if(timeouts_.front()->getRelTicks()<=0)
+        {
+            timeouts_.front()->getBehavior()->pushEvent(timeouts_.front(),true);
+            timeouts_.pop_front();
+        }
+        else
+        {
+            timeouts_.front()->substractFromRelTicks(tickInterval_);
+        }
     }
+
 }
 void XFTimeoutManager::addTimeout(XFTimeout *pNewTimeout)
 {
