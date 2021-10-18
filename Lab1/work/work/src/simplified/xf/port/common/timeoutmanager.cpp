99b05da656d1c8565e0a01c0666d0b25e3562adf
SuperBeurk <58221021+SuperBeurk@users.noreply.github.com>
2021-10-18 13:21:15 +0200

    test 1 and 2 done

--- Lab1/work/work/src/simplified/xf/port/common/timeoutmanager.cpp
+++ Lab1/work/work/src/simplified/xf/port/common/timeoutmanager.cpp
@@ -31,32 +31,28 @@ void XFTimeoutManager::start(std::function<void (uint32_t)> startTimeoutManagerT
 void XFTimeoutManager::scheduleTimeout(int32_t timeoutId, int32_t interval, interface::XFBehavior *pBehavior)
 {
      //Smart add
-     int sub=0;
+     XFTimeout* pNewTimeOut = new XFTimeout(timeoutId,interval,pBehavior);
      if(timeouts_.empty())
      {
-         timeouts_.push_front(new XFTimeout(timeoutId,interval,pBehavior));//Add first element
+         timeouts_.push_front(pNewTimeOut);//Add first element
      }
      else //If we have at lest one element in the list
      {
          for(std::list<XFTimeout *>::iterator it=timeouts_.begin();it!=timeouts_.end();it++)//Parcour the list
          {
-             if((*it)->getRelTicks()>interval)//Insert in middle of the list
+             if((*it)->getRelTicks()>(pNewTimeOut->getRelTicks()))//Insert in middle of the list
              {
-                 it=timeouts_.insert(it,new XFTimeout(timeoutId,interval,pBehavior));//Add new Timeout
-                 for(;it!=timeouts_.end();it++)//Set all tick after this element
-                 {
-                     (*it)->setRelTicks((*it)->getRelTicks()-sub);
-                     sub+=(*it)->getRelTicks();
-                 }
+                 it=timeouts_.insert(it,pNewTimeOut);//Add new Timeout
+                 (*(++it))->substractFromRelTicks((*(--it))->getRelTicks());
                  return;
+
              }
              else
              {
-                 sub+=(*it)->getRelTicks();
+                 pNewTimeOut->substractFromRelTicks((*it)->getRelTicks());
              }
          }
-         timeouts_.push_back(new XFTimeout(timeoutId,interval,pBehavior));
-         timeouts_.back()->setRelTicks(timeouts_.back()->getRelTicks()-sub);
+         timeouts_.push_back(pNewTimeOut);
 
      }
 
@@ -69,17 +65,10 @@ void XFTimeoutManager::tick()
 {
     if(timeouts_.empty()==false)
     {
-        for(std::list<XFTimeout *>::iterator it=timeouts_.begin();it!=timeouts_.end();)
+        if(timeouts_.front()->getRelTicks()<=0)
         {
-            if((*it)->getRelTicks()<=0)
-            {
-                (*it)->getBehavior()->pushEvent((*it),true);
-                it=timeouts_.erase(it);
-            }
-            else
-            {
-                it++;
-            }
+            timeouts_.front()->getBehavior()->pushEvent(timeouts_.front(),true);
+            timeouts_.pop_front();
         }
         timeouts_.front()->substractFromRelTicks(tickInterval_);
     }
